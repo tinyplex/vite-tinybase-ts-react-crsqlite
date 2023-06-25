7a4cf799937d7bf55f4fbe8513ca8dabaa5ad9a4
James Pearce <github@tripleodeon.com>
2023-06-25 10:59:43 -0400

    [tinybase] Read from TinyBase instead of database

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,41 @@
 import "./App.css";
 import { useCallback } from "react";
+import { createStore } from "tinybase";
+import { createCrSqliteWasmPersister } from "tinybase/persisters/persister-cr-sqlite-wasm";
 import {
-  CtxAsync,
-  useCachedState,
-  useDB,
-  useQuery,
-  useSync,
-} from "@vlcn.io/react";
+  CellProps,
+  RowProps,
+  SortedTableView,
+  useCell,
+  useCreatePersister,
+  useCreateStore,
+} from "tinybase/ui-react";
+import { DB } from "@vlcn.io/crsqlite-wasm";
+import { useDB } from "@vlcn.io/react";
 import reactLogo from "./assets/react.svg";
 import tinybaseLogo from "./assets/tinybase.svg";
 import vlcnLogo from "./assets/vlcn.png";
 import randomWords from "./support/randomWords.js";
-import SyncWorker from "./sync-worker.js?worker";
 
-type TestRecord = { id: string; name: string };
 const wordOptions = { exactly: 3, join: " " };
 
-function getEndpoint() {
-  let proto = "ws:";
-  const host = window.location.host;
-  if (window.location.protocol === "https:") {
-    proto = "wss:";
-  }
-
-  return `${proto}//${host}/sync`;
-}
-
-const worker = new SyncWorker();
 function App({ dbname }: { dbname: string }) {
   const ctx = useDB(dbname);
-  useSync({
-    dbname,
-    endpoint: getEndpoint(),
-    room: dbname,
-    worker,
-  });
-  const data = useQuery<TestRecord>(
-    ctx,
-    "SELECT * FROM test ORDER BY id DESC"
-  ).data;
+  const store = useCreateStore(createStore);
+  useCreatePersister(
+    store,
+    (store) =>
+      createCrSqliteWasmPersister(store, ctx.db as DB, {
+        mode: "tabular",
+        tables: {
+          load: { test: { tableId: "test", rowIdColumnName: "id" } },
+        },
+      }),
+    [ctx.db],
+    async (persister) => {
+      await persister.startAutoLoad();
+    }
+  );
 
   const addData = useCallback(() => {
     ctx.db.exec("INSERT INTO test (id, name) VALUES (?, ?);", [
@@ -78,14 +75,12 @@ function App({ dbname }: { dbname: string }) {
             </tr>
           </thead>
           <tbody>
-            {data.map((row) => (
-              <tr key={row.id}>
-                <td>{row.id}</td>
-                <td>
-                  <EditableItem ctx={ctx} id={row.id} value={row.name} />
-                </td>
-              </tr>
-            ))}
+            <SortedTableView
+              store={store}
+              tableId="test"
+              descending={true}
+              rowComponent={DataRow}
+            />
           </tbody>
         </table>
         <p>
@@ -99,40 +94,29 @@ function App({ dbname }: { dbname: string }) {
           to test sync.
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite, React and Vulcan logos to learn more
-      </p>
+      <p className="read-the-docs">Click on the logos to learn more</p>
     </>
   );
 }
 
-function EditableItem({
-  ctx,
-  id,
-  value,
-}: {
-  ctx: CtxAsync;
-  id: string;
-  value: string;
-}) {
-  // Generally you will not need to use `useCachedState`. It is only required for highly interactive components
-  // that write to the database on every interaction (e.g., keystroke or drag) or in cases where you want
-  // to de-bounce your writes to the DB.
-  //
-  // `useCachedState` will never be required once when one of the following is true:
-  // a. We complete the synchronous Reactive SQL layer (SQLiteRX)
-  // b. We figure out how to get SQLite-WASM to do a write + read round-trip in a single event loop tick
-  const [cachedValue, setCachedValue] = useCachedState(value);
-  const onChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
-    setCachedValue(e.target.value);
-    // You could de-bounce your write to the DB here if so desired.
-    return ctx.db.exec("UPDATE test SET name = ? WHERE id = ?;", [
-      e.target.value,
-      id,
-    ]);
-  };
+function DataRow({ store, tableId, rowId }: RowProps) {
+  return (
+    <tr key={rowId}>
+      <td>{rowId}</td>
+      <td>
+        <NonEditableItem
+          store={store}
+          tableId={tableId}
+          rowId={rowId}
+          cellId="name"
+        />
+      </td>
+    </tr>
+  );
+}
 
-  return <input type="text" value={cachedValue} onChange={onChange} />;
+function NonEditableItem({ store, tableId, rowId, cellId }: CellProps) {
+  return useCell(tableId, rowId, cellId, store);
 }
 
 export default App;
